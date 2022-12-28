22cfc7973c3b920d5ceac23189afa4d804b3be25
Rietiefen <115991935+Rietiefen@users.noreply.github.com>
2022-12-28 22:30:32 +0100

    react bootstrap imported

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Searchbox from "./Searchbox";
 import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.css";
 // import Weekly from "./Weekly";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
